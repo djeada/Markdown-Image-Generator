f2e9996d44ef8d7a65d36a8068461eccf8011793
djeada <addjellouli1@gmail.com>
2024-02-05 12:24:54 +0100

    [unit-tests]: fix failling test

--- tests/test_utils/test_other.py
+++ tests/test_utils/test_other.py
@@ -4,12 +4,12 @@ from src.utils.other import hex_to_rgba
 
 
 def test_valid_hex_color_codes():
-    assert hex_to_rgba("#FF0000") == (255, 0, 0, 255)
-    assert hex_to_rgba("#00FF00") == (0, 255, 0, 255)
-    assert hex_to_rgba("#0000FF") == (0, 0, 255, 255)
-    assert hex_to_rgba("#FFFFFF") == (255, 255, 255, 255)
-    assert hex_to_rgba("#000000") == (0, 0, 0, 255)
-    assert hex_to_rgba("#123456") == (18, 52, 86, 255)
+    assert hex_to_rgba("#FF0000") == (255, 0, 0)
+    assert hex_to_rgba("#00FF00") == (0, 255, 0)
+    assert hex_to_rgba("#0000FF") == (0, 0, 255)
+    assert hex_to_rgba("#FFFFFF") == (255, 255, 255)
+    assert hex_to_rgba("#000000") == (0, 0, 0)
+    assert hex_to_rgba("#123456") == (18, 52, 86)
     assert hex_to_rgba("#12345678") == (18, 52, 86, 120)
 
 
